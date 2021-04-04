b1de1b46b2e49a50def8d6d0d7819e123588c466
pylinx64 <77532340+pylinx64@users.noreply.github.com>
2021-04-04 15:40:25 +0300

    Add files via upload

--- sun_python_14/client.py
+++ sun_python_14/client.py
@@ -12,15 +12,45 @@ def receving (name, sock, switch):
 		try:
 			while True:
 				data, addr = sock.recvfrom(1024)
+				print(addr)
 				print('\n'+data.decode("utf-8"))
 				time.sleep(0.2)
 		except:
 			pass
 
 # вкл/выкл чат
-shutdown = False
 join = False
 
 # ваш ip, ваш порт
-host = 
+host = socket.gethostbyname(socket.gethostname())
 port = 0
+
+# ip сервера и порт
+server = ("192.168.1.206", 11719)
+
+# сокет 
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.bind((host, port))
+s.setblocking(0)
+
+name = input('name -> ')
+name = Fore.CYAN + name + Fore.RESET
+# отправляет сообщение на сервер
+s.sendto(("["+name+"] => join chat ").encode("utf-8"), server)
+
+shutdown = False
+rT = threading.Thread(target = receving, args = ("RecvThread", s, shutdown))
+rT.start()
+
+while shutdown == False:
+	try:
+		message = input("["+name+"] > ")
+		message = Fore.YELLOW + message + Fore.RESET
+		if message != '':
+			s.sendto(("["+name+"] > " + message).encode("utf-8"), server)
+	except:
+		s.sendto(("["+name+"] <= left chat ").encode("utf-8"), server)
+		shutdown = True
+		
+rT.join()
+s.close()
